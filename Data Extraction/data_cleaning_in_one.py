519b6279a73b718ff1c85d584c52de2594d241de
kenny <wuyuhao2019@126.com>
2022-04-24 15:39:27 +0800

    调换部分处理顺序

--- Data Extraction/data_cleaning_in_one.py
+++ Data Extraction/data_cleaning_in_one.py
@@ -14,14 +14,13 @@ def deEmojify(text):
     return regrex_pattern.sub(r'',text)
 
 def filter_punc(text):
-    punc = '!"#$%&\'()*+-/:;<=>?@[\\]^_`{|}~'
+    punc = '"$%&\'()*+-/:<=>?@[\\]^_`{|}~'
     temp = ''.join([c for c in text if ord(c)<128])
     return temp.translate(str.maketrans('', '', punc))
 
 def run_processing_steps(filename):
     tweets_df = pd.read_csv(filename, lineterminator='\n')
-    tweets_df.drop_duplicates(inplace=True)
-    tweets_df.dropna(subset=['content'], inplace=True)
+
     tmp = tweets_df.content.str.findall("#\w+")
     # print(tmp[tmp.apply(lambda x: x!=[])]) # 查看存在#的行
 
@@ -45,19 +44,24 @@ def run_processing_steps(filename):
 
     # 2 并移除# 【但保留word】 因为【很多#，是内容的一部分】
     # tweets_df.content = tweets_df.content.str.replace(r"#\w+", "", regex=True)
-    tweets_df.content = tweets_df.content.str.replace(r"#", "", regex=True)
+    tweets_df.content = tweets_df.content.str.replace(r"#", " ", regex=True)
     
     tweets_df.content = tweets_df.content.apply(lambda x: deEmojify(x))
     tweets_df.content = tweets_df.content.str.replace(r'\s{2,}', " ", regex=True)
     tweets_df.content = tweets_df.content.apply(filter_punc)
+    
+    # 处理完所有的 再drop
+    tweets_df.drop_duplicates(inplace=True)
+    tweets_df.dropna(subset=['content'], inplace=True)
     return tweets_df
 
 def main(start, end, path):
     dates = list(pd.date_range(start, end, freq='1D').strftime("%m.%d"))
+    print(dates)
     Country = ['Russia', 'Ukraine']
     #path = '../../Project/'
     for c in Country:
         for i in dates:
             file = f"en_{i}_{c}.csv"
             df = run_processing_steps(path+file)
-            df.to_csv(f"processed_en_{i}_{c}.csv",index=False)
+            df.to_csv(f"./process/processed_en_{i}_{c}.csv",index=False)
