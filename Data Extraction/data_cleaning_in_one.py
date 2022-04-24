b13508f268922f6bb17aa5fb1721cfab12f52582
kenny <wuyuhao2019@126.com>
2022-04-24 15:46:39 +0800

    调整了dropna的顺序

--- Data Extraction/data_cleaning_in_one.py
+++ Data Extraction/data_cleaning_in_one.py
@@ -2,6 +2,7 @@ import pandas as pd
 from cleantext import clean
 import re
 import datetime
+from tqdm import tqdm
 
 # 我的cleantext包没有no_emoji的arg, 换了个方式去除emoji
 def deEmojify(text):
@@ -20,7 +21,8 @@ def filter_punc(text):
 
 def run_processing_steps(filename):
     tweets_df = pd.read_csv(filename, lineterminator='\n')
-
+    tweets_df.drop_duplicates(inplace=True)
+    
     tmp = tweets_df.content.str.findall("#\w+")
     # print(tmp[tmp.apply(lambda x: x!=[])]) # 查看存在#的行
 
@@ -50,18 +52,17 @@ def run_processing_steps(filename):
     tweets_df.content = tweets_df.content.str.replace(r'\s{2,}', " ", regex=True)
     tweets_df.content = tweets_df.content.apply(filter_punc)
     
-    # 处理完所有的 再drop
-    tweets_df.drop_duplicates(inplace=True)
+    # 处理完所有的 再drop nan
     tweets_df.dropna(subset=['content'], inplace=True)
     return tweets_df
 
 def main(start, end, path):
     dates = list(pd.date_range(start, end, freq='1D').strftime("%m.%d"))
-    print(dates)
+    # print(dates)
     Country = ['Russia', 'Ukraine']
     #path = '../../Project/'
     for c in Country:
-        for i in dates:
+        for i in tqdm(dates):
             file = f"en_{i}_{c}.csv"
             df = run_processing_steps(path+file)
-            df.to_csv(f"./process/processed_en_{i}_{c}.csv",index=False)
+            df.to_csv(f"processed_en_{i}_{c}.csv",index=False)
